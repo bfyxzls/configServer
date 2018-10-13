fa9862ec638e12697b6aed275f91d2141a853b63
zhangzl <zhangzl@i-counting.cn>
2018-10-13 15:18:53 +0800

    modify yml

--- src/main/java/lind/configServer/ConfigServerApplication.java
+++ src/main/java/lind/configServer/ConfigServerApplication.java
@@ -1,5 +1,6 @@
 package lind.configServer;
 
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
