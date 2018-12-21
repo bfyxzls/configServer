5bff3a3d70989e90626a470ac67ce337f898d18a
zhangzl <zhangzl@i-counting.cn>
2018-12-21 11:52:22 +0800

    添加了加密信息

--- src/main/java/lind/configServer/ConfigServerApplication.java
+++ src/main/java/lind/configServer/ConfigServerApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
-@EnableDiscoveryClient
 @EnableConfigServer
 @SpringBootApplication
 public class ConfigServerApplication {
